909ec55b9bbea30a84438af1440babb909f2906a
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2016-08-25 15:41:16 +0300

    nodeschoolistanbul eklendi

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use('/bower', express.static('./bower_components'))
 app.get('/', (req, res) => res.render('views/nodetr.html', {page:'nodetr'}))
 app.get('/ilanlar', (req, res) => res.render('views/ilanlar.html', {page:"ilanlar", jobs: require('./data/jobs.json')}))
 app.get('/kitaplar', (req, res) => res.render('views/kitaplar.html', {page:"kitaplar", books: require('./data/books.json')}))
+app.get('/organizasyonlar', (req, res) => res.render('views/organizasyonlar.html', {page:"organizasyonlar"}))
 app.get('/kullanan-sirketler', (req, res) => res.render('views/usedby.html', {page:'usedby'}))
 
 // Server
