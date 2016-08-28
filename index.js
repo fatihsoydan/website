b856f865e9bcb7ee57b44557508fc11a2172ef8f
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2016-08-28 11:27:56 +0300

    İlanlar şirket adına göre sıralandı

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Deps
+const _ = require('lodash')
 const express = require('express')
 const http = require('http')
 const nunjucks = require('nunjucks')
@@ -16,7 +17,10 @@ app.use(express.static('./public'))
 app.use('/bower', express.static('./bower_components'))
 
 app.get('/', (req, res) => res.render('views/nodetr.html', {page:'nodetr'}))
-app.get('/ilanlar', (req, res) => res.render('views/ilanlar.html', {page:"ilanlar", jobs: require('./data/jobs.json')}))
+app.get('/ilanlar', (req, res) => res.render('views/ilanlar.html', {
+  page:"ilanlar",
+  jobs: _.orderBy(require('./data/jobs.json'), ['employer', 'ASC'])
+}))
 app.get('/kitaplar', (req, res) => res.render('views/kitaplar.html', {page:"kitaplar", books: require('./data/books.json')}))
 app.get('/organizasyonlar', (req, res) => res.render('views/organizasyonlar.html', {page:"organizasyonlar"}))
 app.get('/kullanan-sirketler', (req, res) => res.render('views/usedby.html', {page:'usedby'}))
