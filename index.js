c2b2f7ce2799ea13473f4a37aa6913dd10c476bd
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2016-08-29 21:03:49 +0300

    Switched back to Bootstrap

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // Deps
+
 const express = require('express')
 const http = require('http')
 const nunjucks = require('nunjucks')
 const path = require('path')
 
+// App
+
 const app = express()
+
+// Templating
 
 nunjucks.configure(path.join(__dirname, '.'), {
   autoescape: false,
@@ -12,31 +17,59 @@ nunjucks.configure(path.join(__dirname, '.'), {
   noCache: true
 })
 
-app.use(express.static('./public'))
-app.use('/bower', express.static('./bower_components'))
+// Serving static
 
-app.get('/', (req, res) => res.render('views/nodetr.html', {page:'nodetr'}))
-app.get('/iletisim', (req, res) => res.render('views/iletisim.html', {page:'iletisim'}))
+app.use(express.static('./public'))
+
+// Routes
+
+app.get('/', (req, res) => res.render('views/nodetr.html', {
+  title: 'Node.js İstanbul topluluğu',
+  page: 'nodetr'
+}))
+
 app.get('/bilgiler', (req, res) => res.render('views/bilgiler.html', {
-  page:"bilgiler",
+  title: 'Node.js bilgileri',
+  page: 'bilgiler',
   templateengines: require('./data/templateengines.json')
 }))
+
 app.get('/gelistiriciler', (req, res) => res.render('views/gelistiriciler.html', {
-  page:"gelistiriciler",
+  title: 'İstanbul ve Türkiye\'deki Node.js geliştiricileri',
+  page: 'gelistiriciler',
   developers: require('./data/developers.json')
 }))
+
 app.get('/ilanlar', (req, res) => res.render('views/ilanlar.html', {
-  page:"ilanlar",
+  title: 'İstanbul Node.js iş ilanları',
+  page: 'ilanlar',
   jobs: require('./data/jobs.json')
 }))
-app.get('/kitaplar', (req, res) => res.render('views/kitaplar.html', {page:"kitaplar", books: require('./data/books.json')}))
-app.get('/organizasyonlar', (req, res) => res.render('views/organizasyonlar.html', {page:"organizasyonlar"}))
+
+app.get('/iletisim', (req, res) => res.render('views/iletisim.html', {
+  title: 'node.ist ile iletişim',
+  page: 'iletisim'
+}))
+
+app.get('/kitaplar', (req, res) => res.render('views/kitaplar.html', {
+  title: 'Node.js ile ilgili kitaplar',
+  page: 'kitaplar',
+  books: require('./data/books.json')
+}))
+
+app.get('/organizasyonlar', (req, res) => res.render('views/organizasyonlar.html', {
+  title: 'Türkiye\'deki Node.js organizasyonları',
+  page: 'organizasyonlar'
+}))
+
 app.get('/kullanan-sirketler', (req, res) => res.render('views/usedby.html', {
+  title: 'Node.js kullanan şirketler',
   page:'usedby',
   usedbys: require('./data/usedby.json')
 }))
 
 // Server
+
 const server = http.createServer(app)
 const port = process.env.PORT || 8085
 
